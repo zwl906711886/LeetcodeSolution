919785eb883beb9cb5bb41fb6c7adeb05472c0f9
zwling <zwling@hnu.edu.cn>
2022-07-20 16:04:17 +0800

    leetcode_1260

---
+++ src/Leetcode_1260.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode_1260 {
+
+}
+
+class Solution {
+
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int n = grid.length;
+        int m = grid[0].length;
+        List<List<Integer>> ans = new ArrayList<>();
+        int[][] res = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int idx = i * m + j + k;
+                idx %= (n * m);
+                int row = idx / m, col = idx % m;
+                res[row][col] = grid[i][j];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            List<Integer> ret = new ArrayList<>();
+            for (int j = 0; j < m; j++) {
+                ret.add(res[i][j]);
+            }
+            ans.add(ret);
+        }
+        return ans;
+    }
+}
