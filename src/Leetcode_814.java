b953f9a1c7b90a9e3312f631b7a9b36956fd1110
zwling <zwling@hnu.edu.cn>
2022-07-21 11:14:29 +0800

    leetcode_814

---
+++ src/Leetcode_814.java
@@ -0,0 +1,26 @@
+public class Leetcode_814 {
+}
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+}
+@SuppressWarnings({"all"})
+class pruneTree {
+
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) return null;
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if (root.left == null && root.right == null && root.val == 0) return null;
+        return root;
+    }
+}
